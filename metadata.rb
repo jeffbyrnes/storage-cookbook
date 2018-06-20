2d18c25eaa49b8e98480522d6072ff20777be2e2
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-06-20 11:00:16 -0400

    Update previous & current Ubuntu LTS platforms

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '6.0.1'
 chef_version     '>= 12.11', '< 15.0.0'
 
-supports 'ubuntu', '>= 12.04'
+supports 'ubuntu', '>= 16.04'
 
 depends 'ohai'
 depends 'aws', '~> 4.0'
