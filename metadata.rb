3addb060f9ed0a3133e0b9763182a712f118899a
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-06-22 16:47:43 -0400

    Update chef_version pin
    We should continue to work with recent Chef 12 versions.

--- metadata.rb
+++ metadata.rb
@@ -5,9 +5,9 @@ license          'Apache-2.0'
 description      'Installs/Configures storage'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '6.0.1'
+chef_version     '>= 12.11', '< 15.0.0'
 
 supports 'ubuntu', '>= 12.04'
-chef_version '~> 12.10'
 
 depends 'ohai'
 depends 'et_fog', '~> 4.0'
