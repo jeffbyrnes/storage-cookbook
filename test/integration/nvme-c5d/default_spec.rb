72c76d66c7c20aadd82a2af8a479295b10a32897
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-12-07 12:54:10 -0500

    Update test assertion to reasonably pass
    As the cookbook is written right now, it is reasonable to not care what
    the value is for the device name, so long as one is mounted.

--- test/integration/nvme-c5d/default_spec.rb
+++ test/integration/nvme-c5d/default_spec.rb
@@ -3,14 +3,10 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-{
-  '/dev/nvme1n1' => '/mnt/dev0',
-}.each do |device, mountpoint|
-  describe mount mountpoint do
-    its('device') { should eq device }
-    its('type') { should eq 'ext4' }
-    its('options') { should eq ['rw', 'relatime', 'data=ordered'] }
-  end
+describe mount '/mnt/dev0' do
+  its('device') { should match %r{/dev/nvme\dn1} }
+  its('type') { should eq 'ext4' }
+  its('options') { should eq ['rw', 'relatime', 'data=ordered'] }
 end
 
 describe mount '/mnt' do
