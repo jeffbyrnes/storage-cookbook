3a5483739c2d0efe1598169ddd73967827985cfd
Jeff Byrnes <thejeffbyrnes@gmail.com>
2019-12-31 10:36:00 -0500

    Add testing on Chef 15
    TBD if it works yet

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jeff@darksky.net'
 license          'Apache-2.0'
 description      'Installs/Configures storage'
 version          '8.0.0'
-chef_version     '>= 13.0', '< 15.0.0'
+chef_version     '>= 13.0', '< 16.0.0'
 
 source_url       'https://github.com/darkskyapp/storage-cookbook'
 issues_url       'https://github.com/darkskyapp/storage-cookbook/issues'
