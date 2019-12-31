bce91e6176b2385dde5aa49a1769730496e830bb
Jeff Byrnes <thejeffbyrnes@gmail.com>
2019-12-31 10:36:24 -0500

    Update maintainer email & metadata URLs

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
 name             'storage'
 maintainer       'Jeff Byrnes'
-maintainer_email 'jeff@darksky.net'
+maintainer_email 'thejeffbyrnes@gmail.com'
 license          'Apache-2.0'
 description      'Installs/Configures storage'
 version          '8.0.0'
 chef_version     '>= 13.0', '< 16.0.0'
 
-source_url       'https://github.com/darkskyapp/storage-cookbook'
-issues_url       'https://github.com/darkskyapp/storage-cookbook/issues'
+source_url       'https://github.com/jeffbyrnes/storage-cookbook'
+issues_url       'https://github.com/jeffbyrnes/storage-cookbook/issues'
 
 supports 'ubuntu', '>= 16.04'
 
